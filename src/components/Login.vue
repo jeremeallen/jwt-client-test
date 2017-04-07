429980ced61aef758fc203a37ddef94d60975e4f
Jereme Allen <jereme.allen@wheniwork.com>
2017-04-07 13:41:25 -0500

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="doLogin">
+    <label>Email</label>
+    <input type='text' v-model='user.email'/>
+
+    <label>Password</label>
+    <input type='password' v-model='user.password'/>
+
+    <button>Login</button>
+  </form>
+</template>
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        user: {
+          email: '',
+          password: '',
+        },
+      };
+    },
+  };
+</script>
